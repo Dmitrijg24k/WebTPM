adddc443fcd0fede7b03da2bf8d6d8c79895dcb0
Dmitrijg24k <dmitrij3e5g@gmail.com>
2022-03-10 11:46:05 +0400

    Routing configuration in ASGI

--- WebTPMG/settings.py
+++ WebTPMG/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +42,9 @@ INSTALLED_APPS = [
 
     'chat',
 ]
+
+ASGI_APPLICATION = 'WebTPMG.asgi.application'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

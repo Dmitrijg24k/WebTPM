00c264efa12aaec73fa91c0f5aa8fa8a7eec82a4
Dmitrijg24k <dmitrij3e5g@gmail.com>
2022-03-27 14:37:48 +0400

    End sync One to One

--- tpm/consumers.py
+++ tpm/consumers.py
@@ -4,17 +4,110 @@ from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 import numpy as np
 import math
+import random
 from random import randrange
 
+class Machine:
+    def __init__(self, k=3, n=4, l=6):
+        self.k = k
+        self.n = n
+        self.l = l
+        self.W = np.random.randint(-l, l + 1, [k, n])
+
+    def get_output(self, X):
+        '''
+        Returns a binary digit tau for a given random vecor.
+        Arguments:
+        X - Input random vector
+        '''
+        k = self.k
+        n = self.n
+        W = self.W
+        X = X.reshape([k, n])
+        sigma = np.sign(np.sum(X * W, axis=1)) # Compute inner activation sigma Dimension:[K]
+        tau = np.prod(sigma) # The final output
+        self.X = X
+        self.sigma = sigma
+        self.tau = tau
+        return tau
+    def __call__(self, X):
+        return self.get_output(X)
+    def update(self, tau2, update_rule='hebbian'):
+        '''
+        Updates the weights according to the specified update rule.
+        Arguments:
+        tau2 - Output bit from the other machine;
+        update_rule - The update rule. 
+        Should be one of ['hebbian', 'anti_hebbian', random_walk']
+        '''
+        X = self.X
+        tau1 = self.tau
+        sigma = self.sigma
+        W = self.W
+        l = self.l
+        if (tau1 == tau2):
+            if update_rule == 'hebbian':
+                self.hebbian(W, X, sigma, tau1, tau2, l)
+                # for (i, j), _ in np.ndenumerate(self.W):
+                #     self.W[i, j] += self.X[i, j] * self.tau * self.theta(self.sigma[i], self.tau) * self.theta(self.tau, tau2)
+                #     self.W[i, j] = np.clip(self.W[i, j] , -1 * (self.l), self.l)
+            elif update_rule == 'anti_hebbian':
+                self.anti_hebbian(W, X, sigma, tau1, tau2, l)
+            elif update_rule == 'random_walk':
+                self.random_walk(W, X, sigma, tau1, tau2, l)
+            else:
+                raise Exception("Invalid update rule. Valid update rules are: " + 
+                    "\'hebbian\', \'anti_hebbian\' and \'random_walk\'.")
+    def theta(self,t1, t2):
+        return 1 if t1 == t2 else 0
+    def hebbian(self, W, X, sigma, tau1, tau2, l):
+        k, n = W.shape
+        for (i, j), _ in np.ndenumerate(W):
+            W[i, j] += X[i, j] * tau1 * self.theta(sigma[i], tau1) * self.theta(tau1, tau2)
+            W[i, j] = np.clip(W[i, j] , -l, l)
+        self.W = W
+    def anti_hebbian(self, W, X, sigma, tau1, tau2, l):
+        k, n = W.shape
+        for (i, j), _ in np.ndenumerate(W):
+            W[i, j] -= X[i, j] * tau1 * self.theta(sigma[i], tau1) * self.theta(tau1, tau2)
+            W[i, j] = np.clip(W[i, j], -l, l)
+        self.W = W
+    def random_walk(self, W, X, sigma, tau1, tau2, l):
+        k, n = W.shape
+        for (i, j), _ in np.ndenumerate(W):
+            W[i, j] += X[i, j] * self.theta(sigma[i], tau1) * self.theta(tau1, tau2)
+            W[i, j] = np.clip(W[i, j] , -l, l)
+        self.W = W
+    def chaosmap(self):
+        print(self.W)
+        r = sum(list(np.hstack(self.W)))
+        print("r-", r)
+        rr = sum([abs(x) for x in (list(np.hstack(self.W)))])
+        print("rr-", rr)
+        t = float(abs(r)) / float(rr)
+        print("t-", t)
+        x = t
+        for i in range(rr):
+            x = (3.6 + t/2)* x *(1 - x)
+        print("x-", x)
+        return x
+
+def gen_rand_vector(sid, l, k, n): #сид, диапазон, количество нейронов, количество входов
+    np.random.seed(sid)
+    #print('sid, vector', sid, np.random.randint(-l, l + 1, [k, n]))
+    return np.random.randint(-l, l + 1, [k, n])
+def gen_sid():
+    return random.randint(0, 1000)
 
 class TpmConsumer(WebsocketConsumer):
     # let ServerTPM = 
     def connect(self):
         self.accept()
-        self.send(text_data=json.dumps({
-            'type': 'ServerReadySync',
-            'message': 'Client connected'
-        }))
+        # self.send(text_data=json.dumps({
+        #     'type': 'ServerReadySync',
+        #     'message': 'Client connected'
+        # }))
+        print("client wants to connect")
         # self.room_group_name = 'text'
 
         # async_to_sync(self.channel_layer.group_add)(
@@ -24,45 +117,98 @@ class TpmConsumer(WebsocketConsumer):
         # self.accept()
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        # message = text_data_json['message']
         #print("connect0", message)
+        if text_data_json['type'] == 'ClientReadySync':
+            print(text_data_json)
+            # message = text_data_json['message']
+            # self.ServerTPM = TPMClient(text_data_json['k'],text_data_json['n'], text_data_json['l'])
+            self.send(text_data=json.dumps({
+                'type': 'ServerReadySync',
+                'message': 'Server is ready'
+            }))
         if text_data_json['type'] == 'GetParametrs':
-            print("connect1")
-            message = text_data_json['message']
-            self.ServerTPM = TPMClient(text_data_json['k'],text_data_json['n'], text_data_json['l'])
+            print(text_data_json)
+            # message = text_data_json['message']
+            self.timer = 0
+            self.k = text_data_json['k']
+            self.n = text_data_json['n']
+            self.l = text_data_json['l']
+            self.ServerTPM = Machine(text_data_json['k'],text_data_json['n'], text_data_json['l'])
+            self.sid = gen_sid()
+            # vector = []
+            self.ServerTPM.X = gen_rand_vector(self.sid, self.l, self.k, self.n)
+            # for i in range(len(X)):
+            #     vector.append([])
+            #     for j in range(len(X[i])):
+            #         vector[i].append(X[i][j])
             self.send(text_data=json.dumps({
-                'type': 'start',
-                'message': message
+                'type': 'GetFirstSid',
+                'sid': self.sid,
+                'vector': self.ServerTPM.X.tolist()
             }))
-        if text_data_json['type'] == 'weights':
-            # print("connect2", self.ServerTPM.tpm.k)
-            message = text_data_json['message']
-            self.ServerTPM.receive_vector(text_data_json['list_vec'])
-            self.ServerTPM.send_output(self)
-            if self.ServerTPM.tpm.out == text_data_json['output']:
-                self.ServerTPM.tpm.update_weights(text_data_json['output'])
-            # self.send(text_data=json.dumps({
-            #     'type': 'chat',
-            #     'message': message
-            # }))
-        if text_data_json['type'] == 'receive_chaos_output':
-            print("connect3", text_data_json['output'], self.ServerTPM.tpm.chaosmap())
-            self.send(text_data=json.dumps({
-                'type': 'receive_chaos_output2',
-                'message': message,
-                'output': self.ServerTPM.tpm.chaosmap(),
-            }))
-            if text_data_json['output'] == self.ServerTPM.tpm.chaosmap():
-                self.ServerTPM.IsSync = True
-                print('SUCCESS: synched with Alice')
-                self.disconnect()
-        # async_to_sync(self.channel_layer.group_send)(
-        #     self.room_group_name,
-        #     {
-        #         'type': 'chat_message',
-        #         'message': message
-        #     }
-        # )
+        if text_data_json['type'] == 'GetResultClient':
+            #print(self.ServerTPM.k, self.ServerTPM.n, self.ServerTPM.l)
+            #print("sid:{}".format(self.sid))
+            # self.X = gen_rand_vector(self.sid, self.l, self.k, self.n)
+            #print("Vector:{}".format(self.X))
+            self.resultServer = self.ServerTPM.get_output(self.ServerTPM.X)
+            self.ServerTPM.X = gen_rand_vector(self.sid, self.l, self.k, self.n)
+            #print("resultServer:{}".format(self.resultServer))
+            #print("resultClient:{}".format(text_data_json['resultClient']))
+            #self.ServerTPM.update(self.resultClient, 'hebbian')
+            # message = text_data_json['message']
+            self.sid = gen_sid()
+            if self.resultServer == text_data_json['resultClient']:
+                #print(self.sid)
+                ClientW = np.array(text_data_json['W'])
+                ttt = True
+                for i in range(len(self.ServerTPM.W)):
+                    for j in range(len(self.ServerTPM.W[i])):
+                        if self.ServerTPM.W[i][j] != ClientW[i][j]:
+                            ttt = False
+                if ttt:
+                    print(self.ServerTPM.chaosmap()==text_data_json['chaosmap'])
+                    print('server-', self.ServerTPM.W)
+                    print('client-',np.array(text_data_json['W']))
+                    print("FinishSync:{}".format(self.ServerTPM.W))
+                    self.send(text_data=json.dumps({
+                        'type': 'FinishSync',
+                        'message': 'FinishSync hehe',
+                    }))
+                else:
+                    print(self.sid)
+                    print(self.ServerTPM.X)
+                    print('server-', self.ServerTPM.W)
+                    print('client-',np.array(text_data_json['W']))
+                    print('result1-',self.ServerTPM.tau)
+                    print('result2-',int(text_data_json['resultClient']))
+                    self.ServerTPM.update(int(text_data_json['resultClient']), 'hebbian')
+                    #print(self.sid)
+                    #print(self.ServerTPM.W)
+                    # vector = []
+                    # for i in range(len(self.X)):
+                    #     vector.append([])
+                    #     for j in range(len(self.X[i])):
+                    #         vector[i].append(X[i][j])
+                    self.send(text_data=json.dumps({
+                        'type': 'GetSidAndResultTrue',
+                        'sid': int(self.sid),
+                        'vector': self.ServerTPM.X.tolist(),
+                        "Result": int(self.resultServer),
+                    }))
+            else:
+                # vector = []
+                # for i in range(len(self.X)):
+                #     vector.append([])
+                #     for j in range(len(self.X[i])):
+                #         vector[i].append(X[i][j])
+                self.send(text_data=json.dumps({
+                    'type': 'GetSidAndResultFalse',
+                    'sid': int(self.sid),
+                    'vector': self.ServerTPM.X.tolist(),
+                    "Result": int(self.resultServer),
+                }))                    
     
     def chat_message(self, event):
         message = event['message']
@@ -97,134 +243,3 @@ class TpmConsumer(WebsocketConsumer):
     #     message = event['message']
 
     #     print("Message from client-" + message)
-
-class TPMClient:
-    def __init__(self, k = 16, n = 16, l = 100):
-        self.user = None
-        self.tpm = TPM(k, n, l)
-        self.n = 0
-        self.IsSync = False
-
-    def send_vector_and_output(self, sio):
-        vector = self.rand_vec()
-        list_vec = [np.array(vector[x:x+16]) for x in range(0, len(vector), 16)]
-        self.tpm.get_output(list_vec)
-
-        # sio.emit('weights',
-        #     {
-        #         'msg':'sending random vector and output',
-        #         'vector': vector,
-        #         'output': self.tpm.out,
-        #         # 'sid': tpmclient.partner_sid
-        #     }
-        # )
-        sio.send(text_data=json.dumps({
-            'type': 'weights',
-            'message': 'sending random vector and output',
-            'vector': vector,
-            'output': self.tpm.out,
-        }))
-        self.n += 1
-
-    def receive_vector(self, vector_):
-        vec = [np.array(x) for x in vector_]
-        self.tpm.get_output(vec)
-
-    def rand_vec(self):
-        l = []
-        for i in range(256):
-            l.append(randrange(-100, 100))
-        return l
-
-    def send_output(self, sio): #tut
-        # sio.emit('send_output',
-        #     {
-        #         'msg':'sending output',
-        #         'output': self.tpm.out,
-        #         # 'sid': tpmclient.partner_sid
-        #     }
-        # )
-        sio.send(text_data=json.dumps({
-            'type': 'output_received',
-            'message': 'sending output',
-            'output': self.tpm.out,
-            # 'sid': tpmclient.partner_sid
-        }))
-
-    def send_chaos_output(self):
-        sio.emit('send_chaos_output',
-            {
-                'msg':'sending chaos output',
-                'output': self.tpm.chaosmap(),
-                # 'sid': tpmclient.partner_sid
-            }
-        )
-
-    def save_key(self):
-        re = [abs(item+155) for sublist in self.tpm.weights for item in sublist]
-        key = bytes(abs(x) for x in re).decode('cp437')
-        print(key)
-        # with open("KEYS/{}.txt".format(CHANNEL), "w") as text_file:
-        #     print(key, file=text_file)
-
-
-class TPM:
-    def __init__(self, k_, n_, l_):
-        self.k = k_
-        self.n = n_
-        self.l = l_
-        self.weights = self.initialize_w()
-        self.inputs = None
-        self.H = np.zeros(self.k)
-        self.out = None
-        self.X = None
-
-    def get_output(self, input_):
-        self.X = input_
-        self.out = 1
-
-        for i in range(self.k):
-            self.H[i] = self.signum(np.dot(input_[i], self.weights[i]))
-            self.out *= self.signum(np.dot(input_[i], self.weights[i]))
-
-    def initialize_w(self):
-        p = []
-        for i in range(self.k):
-            p.append(np.random.randint(-self.l, self.l, size=self.n))
-        return p
-
-    def signum(self, x):
-        return math.copysign(1, x)
-
-    def update_weights(self, outputB):
-        for i in range(self.k):
-            for j in range(self.n):
-                self.weights[i][j] += self.X[i][j] * self.out * self.isequal(self.out, self.H[i]) * self.isequal(self.out, outputB)
-                self.weights[i][j] = self.g(self.weights[i][j])
-
-    def isequal(self, A, B):
-        if A==B:
-            return 1.0
-        else:
-            return 0.0
-
-    def g(self, w):
-        if w > self.l:
-            return self.l
-        if w < -self.l:
-            return -self.l
-        else:
-            return w
-
-    def chaosmap(self):
-        r = sum(list(np.hstack(self.weights)))
-        #print('r-', r)
-        rr = sum([abs(x) for x in (list(np.hstack(self.weights)))])
-        #print('rr-', rr)
-        t = float(abs(r)) / float(rr)
-        #print('t-', t)
-        x = t
-        for i in range(rr):
-            x = (3.6 + t/2)* x *(1 - x)
-        #print('x-', x)
-        return x
